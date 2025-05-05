1c2aa9815b6c297b6d1e0b4886c4cce975d1293d
SHRUTHI NAIDU <shruthinaidu91@gmail.com>
2025-05-05 19:39:10 +1000

    fixing issues with app.py
    Signed-off-by: SHRUTHI NAIDU <shruthinaidu91@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
 
-app= Flask(__name__)
-APP_NAME = os.getenv('APP_NAME', 'airtasker')
+app = Flask(__name__)
+APP_NAME = os.getenv("APP_NAME", "airtasker")
+
 
 @app.route("/")
 def root():
     return APP_NAME, 200, {"Content-Type": "text/plain"}
 
+
 @app.route("/healthcheck")
 def healthcheck():
     return "OK", 200, {"Content-Type": "text/plain"}
-
